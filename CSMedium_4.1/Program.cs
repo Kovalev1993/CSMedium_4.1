6d352e137d609f0922fb11a20c3fba5ae0020084
Ковалев Егор Сергеевич <air1618@gmail.com>
2019-10-29 16:11:22 +0300

    Decorator, v2

--- CSMedium_4.1/Program.cs
+++ CSMedium_4.1/Program.cs
@@ -13,88 +13,68 @@ namespace CSMedium_4._1
         {
             new ConsoleLogWriter().WriteError("Запись в консоль");
             new FileLogWriter().WriteError("Запись в файл");
-            new DoubleLogWriter().WriteError("Запись в консоль и файл");
-            new SecureConsoleLogWritter(true).WriteError("Безопасная запись в консоль");
-            new SecureFileWriter(true).WriteError("Безопасная запись в файл");
-            new SecureDoubleWriter(true).WriteError("Безопасная запись в консоль и файл");
+            new DoubleLogWriter(new ConsoleLogWriter(), new FileLogWriter()).WriteError("Запись в консоль и файл");
+            new SecureLogWritter(new ConsoleLogWriter(), true).WriteError("Безопасная запись в консоль");
+            new SecureLogWritter(new FileLogWriter(), true).WriteError("Безопасная запись в файл");
+            new SecureLogWritter(new DoubleLogWriter(new ConsoleLogWriter(), new FileLogWriter()), true).WriteError("Безопасная запись в консоль и файл");
         }
     }
-    
-    class ConsoleLogWriter
+
+    interface ILogger
     {
-        public virtual void WriteError(string message)
+        void WriteError(string message);
+    }
+    
+    class ConsoleLogWriter : ILogger
+    {
+        public void WriteError(string message)
         {
             Console.WriteLine(message);
         }
     }
 
-    class FileLogWriter
+    class FileLogWriter : ILogger
     {
-        public virtual void WriteError(string message)
+        public void WriteError(string message)
         {
             File.WriteAllText("log.txt", message);
         }
     }
 
-    class DoubleLogWriter
+    class DoubleLogWriter : ILogger
     {
+        private ILogger _log0;
+        private ILogger _log1;
+
+        public DoubleLogWriter(ILogger log0, ILogger log1)
+        {
+            _log0 = log0;
+            _log1 = log1;
+        }
+
         public virtual void WriteError(string message)
         {
-            new ConsoleLogWriter().WriteError(message);
-            new ConsoleLogWriter().WriteError(message);
+            _log0.WriteError(message);
+            _log1.WriteError(message);
         }
     }
 
-    class SecureConsoleLogWritter : ConsoleLogWriter
+    class SecureLogWritter : ILogger
     {
+        private ILogger _log;
         private bool _testMode;
 
-        public SecureConsoleLogWritter(bool testMode = false)
+        public SecureLogWritter(ILogger log, bool testMode = false)
         {
+            _log = log;
             _testMode = testMode;
         }
 
-        public override void WriteError(string message)
+        public void WriteError(string message)
         {
             if((int)(DateTime.Now.DayOfWeek) % 2 == 0 || _testMode)
             {
-                new ConsoleLogWriter().WriteError(message);
-            }
-        }
-    }
-
-    class SecureFileWriter : FileLogWriter
-    {
-        private bool _testMode;
-
-        public SecureFileWriter(bool testMode = false)
-        {
-            _testMode = testMode;
-        }
-
-        public override void WriteError(string message)
-        {
-            if((int)(DateTime.Now.DayOfWeek) % 2 == 0 || _testMode)
-            {
-                new FileLogWriter().WriteError(message);
-            }
-        }
-    }
-
-    class SecureDoubleWriter : ConsoleLogWriter
-    {
-        private bool _testMode;
-
-        public SecureDoubleWriter(bool testMode = false)
-        {
-            _testMode = testMode;
-        }
-
-        public override void WriteError(string message)
-        {
-            if((int)(DateTime.Now.DayOfWeek) % 2 == 0 || _testMode)
-            {
-                new DoubleLogWriter().WriteError(message);
+                _log.WriteError(message);
             }
         }
     }
